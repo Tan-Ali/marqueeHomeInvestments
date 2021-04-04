9d499bd347be2dd5961e3c2537f4985e8e1349e4
Tanner Nielsen <tan-ali5@outlook.com>
2021-04-03 22:44:04 -0700

    starting front page

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import Jumbotron from './component/Jumbotron';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Jumbotron>
         <p>
+          
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        </Jumbotron>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ client/src/component/Jumbotron/index.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import logo from '../img/marquee.jpg';
+import './style.css';
+
+class Jumbotron extends Component {
+
+  state = {
+    counter: "18,287"
+  }
+
+  render() {
+    return (
+      <div class="row" id="jumbotron">
+        <div class="col">
+          <div class="row">
+            <div class="col-12 col-md-6 text-center pt-3">
+              <img id="jumbo-logo" src={logo} />
+            </div>
+            <div class="col-12 col-md-6">
+              <div class="row pt-5 py-md-5">
+                <h1 id="jumbo-text">Coffee lovers served...</h1>
+                <h1 id="jumbo-counter">{this.state.counter}</h1>
+              </div>
+              <div class="row text-center">
+                <div class="col-6 col-sm-4 col-lg-3">
+                  <button id="login-btn" class="btn button">Login</button>
+                </div>
+                <div class="col-6 col-sm-4 col-lg-3">
+                  <button id="signup-btn" class="btn button" data-toggle="modal" data-target="#signUpModal">Sign Up</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Jumbotron;
